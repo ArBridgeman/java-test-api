6debb55aeb2fe5ed05c989ac0ffd91f327887c83
Ariel Schulz <ariel.bridgem@gmail.com>
2023-05-20 13:21:40 +0200

    CODE-8 Add replace method in services
    As profiles may be created on the fly, it seems like
    it is desired that a UserProfileChange associated
    with a new UserId would still be saved, instead of
    tossing an error.
    
    Similarly, new UserProfilePropertyNames in the
    'replace' operation should be inserted into the
    UserProfile.

--- src/main/java/com/spotlight/platform/userprofile/api/core/profile/UserProfileService.java
+++ src/main/java/com/spotlight/platform/userprofile/api/core/profile/UserProfileService.java
@@ -1,10 +1,21 @@
 package com.spotlight.platform.userprofile.api.core.profile;
 
+import static com.spotlight.platform.userprofile.api.model.profile.primitives.UserChangeType.*;
+
 import com.spotlight.platform.userprofile.api.core.exceptions.EntityNotFoundException;
 import com.spotlight.platform.userprofile.api.core.profile.persistence.UserProfileDao;
 import com.spotlight.platform.userprofile.api.model.profile.UserProfile;
+import com.spotlight.platform.userprofile.api.model.profile.UserProfileChange;
 import com.spotlight.platform.userprofile.api.model.profile.primitives.UserId;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyName;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyValue;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.el.MethodNotFoundException;
 import javax.inject.Inject;
 
 public class UserProfileService {
@@ -17,5 +28,28 @@ public class UserProfileService {
 
     public UserProfile get(UserId userId) {
         return userProfileDao.get(userId).orElseThrow(EntityNotFoundException::new);
+    }
+
+    private Map<UserProfilePropertyName, UserProfilePropertyValue> getProfileProperties(
+            UserId userId) {
+        Optional<UserProfile> userProfile = userProfileDao.get(userId);
+        if (userProfile.isEmpty()) {
+            // Reduce downstream complexities by providing empty HashMap
+            // in the event that a user profile did not previously exist.
+            return new HashMap<>();
+        }
+        return new HashMap<>(userProfile.get().userProfileProperties());
+    }
+
+    public void replace(UserProfileChange userProfileChange) {
+        UserId userId = userProfileChange.userId();
+        Map<UserProfilePropertyName, UserProfilePropertyValue> profileProperties =
+                getProfileProperties(userId);
+
+        // Replace existing UserProfilePropertyName in profile and add new ones;
+        // It will replace regardless of previous type.
+        profileProperties.putAll(userProfileChange.userProfileProperties());
+
+        userProfileDao.put(new UserProfile(userId, Instant.now(), profileProperties));
     }
 }
--- src/test/java/com/spotlight/platform/userprofile/api/core/profile/UserProfileServiceTest.java
+++ src/test/java/com/spotlight/platform/userprofile/api/core/profile/UserProfileServiceTest.java
@@ -3,18 +3,22 @@ package com.spotlight.platform.userprofile.api.core.profile;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+// TODO figure out way to not change to import star
+import static org.mockito.Mockito.*;
 
 import com.spotlight.platform.userprofile.api.core.exceptions.EntityNotFoundException;
 import com.spotlight.platform.userprofile.api.core.profile.persistence.UserProfileDao;
-import com.spotlight.platform.userprofile.api.model.profile.primitives.UserId;
-import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfileFixtures;
+import com.spotlight.platform.userprofile.api.model.profile.UserProfile;
+import com.spotlight.platform.userprofile.api.model.profile.UserProfileChange;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.*;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 class UserProfileServiceTest {
@@ -41,6 +45,91 @@ class UserProfileServiceTest {
 
             assertThatThrownBy(() -> userProfileService.get(UserProfileFixtures.USER_ID))
                     .isExactlyInstanceOf(EntityNotFoundException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("replace")
+    class Replace {
+        private static final UserProfile UPDATED_USER_PROFILE =
+                new UserProfile(
+                        UserProfileFixtures.USER_ID,
+                        UserProfileFixtures.LATEST_UPDATE_TIMESTAMP,
+                        UserProfileChangeFixture.REPLACED_PROFILE_PROPERTY);
+
+        @Test
+        void replaceExistingProperty_updatesValue() {
+            when(userProfileDaoMock.get(any(UserId.class)))
+                    .thenReturn(Optional.of(UserProfileFixtures.USER_PROFILE));
+
+            try (MockedStatic<Instant> mockedStatic = mockStatic(Instant.class)) {
+                mockedStatic
+                        .when(Instant::now)
+                        .thenReturn(UserProfileFixtures.LATEST_UPDATE_TIMESTAMP);
+                userProfileService.replace(UserProfileChangeFixture.USER_PROFILE_CHANGE);
+            }
+
+            ArgumentCaptor<UserProfile> myCaptor = ArgumentCaptor.forClass(UserProfile.class);
+            verify(userProfileDaoMock).put(myCaptor.capture());
+
+            assertThat(myCaptor.getValue())
+                    .usingRecursiveComparison()
+                    .isEqualTo(UPDATED_USER_PROFILE);
+        }
+
+        @Test
+        void replaceNewProperty_addsValue() {
+            when(userProfileDaoMock.get(any(UserId.class)))
+                    .thenReturn(Optional.of(UserProfileFixtures.USER_PROFILE));
+
+            Map<UserProfilePropertyName, UserProfilePropertyValue> newProfileProperty =
+                    UserProfileChangeFixture.getUserProfileProperty("property2", "property2Value");
+            UserProfileChange userProfileChange =
+                    new UserProfileChange(
+                            UserProfileFixtures.USER_ID,
+                            UserChangeType.REPLACE,
+                            newProfileProperty);
+
+            try (MockedStatic<Instant> mockedStatic = mockStatic(Instant.class)) {
+                mockedStatic
+                        .when(Instant::now)
+                        .thenReturn(UserProfileFixtures.LATEST_UPDATE_TIMESTAMP);
+                userProfileService.replace(userProfileChange);
+            }
+
+            ArgumentCaptor<UserProfile> myCaptor = ArgumentCaptor.forClass(UserProfile.class);
+            verify(userProfileDaoMock).put(myCaptor.capture());
+
+            Map<UserProfilePropertyName, UserProfilePropertyValue> allProperties = new HashMap<>();
+            allProperties.putAll(UserProfileFixtures.USER_PROFILE.userProfileProperties());
+            allProperties.putAll(newProfileProperty);
+            UserProfile updatedUserProfile =
+                    new UserProfile(
+                            UserProfileFixtures.USER_ID,
+                            UserProfileFixtures.LATEST_UPDATE_TIMESTAMP,
+                            allProperties);
+            assertThat(myCaptor.getValue())
+                    .usingRecursiveComparison()
+                    .isEqualTo(updatedUserProfile);
+        }
+
+        @Test
+        void replaceForNonExistingUser_savesNewProfile() {
+            when(userProfileDaoMock.get(any(UserId.class))).thenReturn(Optional.empty());
+
+            try (MockedStatic<Instant> mockedStatic = mockStatic(Instant.class)) {
+                mockedStatic
+                        .when(Instant::now)
+                        .thenReturn(UserProfileFixtures.LATEST_UPDATE_TIMESTAMP);
+                userProfileService.replace(UserProfileChangeFixture.USER_PROFILE_CHANGE);
+            }
+
+            ArgumentCaptor<UserProfile> myCaptor = ArgumentCaptor.forClass(UserProfile.class);
+            verify(userProfileDaoMock).put(myCaptor.capture());
+
+            assertThat(myCaptor.getValue())
+                    .usingRecursiveComparison()
+                    .isEqualTo(UPDATED_USER_PROFILE);
         }
     }
 }
