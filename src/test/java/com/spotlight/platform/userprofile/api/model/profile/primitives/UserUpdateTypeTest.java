384e79267014486718a0ce248dbe23d842187e47
Ariel Schulz <ariel.bridgem@gmail.com>
2023-05-27 11:11:53 +0200

    CODE-11 Add increment type to UserUpdateType and switch to parameterized tests

--- src/test/java/com/spotlight/platform/userprofile/api/model/profile/primitives/UserUpdateTypeTest.java
+++ src/test/java/com/spotlight/platform/userprofile/api/model/profile/primitives/UserUpdateTypeTest.java
@@ -8,22 +8,35 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.spotlight.platform.userprofile.api.core.exceptions.EntityNotFoundException;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class UserUpdateTypeTest {
 
-    @Test
-    void serialization_worksAsExpected() {
-        assertThatJson(UserUpdateType.REPLACE).isEqualTo("replace");
+    @ParameterizedTest
+    @MethodSource("provideParameters")
+    void serialization_worksAsExpected(UserUpdateType enumToSerialize, String expectedJsonValue) {
+        assertThatJson(enumToSerialize).isEqualTo(expectedJsonValue);
     }
 
-    @Test
-    void fromJson_worksAsExpected() {
-        assertThat(UserUpdateType.fromJson("replace")).isEqualTo(UserUpdateType.REPLACE);
+    @ParameterizedTest
+    @MethodSource("provideParameters")
+    void fromJson_worksAsExpected(UserUpdateType expectedEnum, String jsonToSerialize) {
+        assertThat(UserUpdateType.fromJson(jsonToSerialize)).isEqualTo(expectedEnum);
     }
 
     @Test
     void fromJsonForUndefinedValue_throwsException() {
         assertThatThrownBy(() -> UserUpdateType.fromJson("do_something_not_defined"))
                 .isExactlyInstanceOf(EntityNotFoundException.class);
+    }
+
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+                Arguments.of(UserUpdateType.REPLACE, "replace"),
+                Arguments.of(UserUpdateType.INCREMENT, "increment"));
     }
 }
